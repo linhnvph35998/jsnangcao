3ab1a63e7aad615445662d19b6b0bbf0d3cf2638
linhnvph35998 <05042004linhdz@gmail.com>
2024-01-27 14:31:06 +0700

    project

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,14 @@
+console.log(window);
+
+console.dir(window.document);
+
+console.log(document.body);
+console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello from body</h1>';
+
+document.write('Hello from JS');
+
+document.getElementById('main').innerHTML = '<h1>Hello from main</h1>';
+
+document.querySelector('#main h1').innerText = 'Hello';
